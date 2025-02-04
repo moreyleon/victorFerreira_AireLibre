83b2faf73443577b31e4e124aae56c9d1e4d7d51
--add <victor_ferreira@live.com.ar>
2025-02-04 18:34:34 -0300

    agregue los JSON

--- src/app.js
+++ src/app.js
@@ -8,9 +8,6 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productRouter = require('./routes/product');
 var compraRouter = require('./routes/compra');
-//var adminRouter = require('./routes/admin');
-//var addRouter  = require('./routes/productAdd');
-//var aditRouter  = require('./routes/productEdit');
 
 var app = express();
 
@@ -28,9 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/product', productRouter);
 app.use('/compra', compraRouter);
-//app.use('/admin', adminRouter);
-//app.use('/productAdd', addRouter);
-//app.use('/productEdit', aditRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
