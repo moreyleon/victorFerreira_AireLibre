6176e1d70671ad5046f1977961964813131f7c6d
--add <paomori18@gmail.com>
2025-04-13 16:40:29 -0300

    fix validations

--- src/middleware/upImage.js
+++ src/middleware/upImage.js
@@ -4,26 +4,28 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null,path.join(__dirname,'../../public/images/products'));
+    cb(null, path.join(__dirname, "../../public/images/products"));
   },
-  
+
   filename: function (req, file, cb) {
-     const uniqueSuffix = path.basename(file.originalname, path.extname(file.originalname)) + "-" + Date.now() + path.extname(file.originalname) ;
+    const uniqueSuffix =
+      path.basename(file.originalname, path.extname(file.originalname)) +
+      "-" +
+      Date.now() +
+      path.extname(file.originalname);
     cb(null, uniqueSuffix);
-   }
-}); 
+  },
+});
 
 const fileFilter = (req, file, cb) => {
-
   const filtro = /\.(jpg|jpeg|png)$/;
 
   if (filtro.test(file.originalname.toLowerCase())) {
-    
     cb(null, true);
   } else {
-    
-    cb(new Error('Formato de archivo no permitido. Solo se permiten JPG, JPEG y PNG.'));
+    req.errorValidationImage = "No es un tipo de archivo valido";
+    cb(null, false);
   }
 };
 
-module.exports = multer({ storage,fileFilter });
+module.exports = multer({ storage, fileFilter });
